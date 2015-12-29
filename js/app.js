4e053c13565745e8ee8b199e8e1f754c0c20bcba
Jorge Blom-Dahl Oliver <jorgeblom@MBP-BMMX.local>
2015-12-29 22:32:03 +0100

    CRUD de Pacientes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("clinicApp", ["firebase"]);
+var app = angular.module("clinicApp", ["firebase", "ngRoute"]);
 
 app.directive('myEnter', function () {
     return function (scope, element, attrs) {
@@ -12,6 +12,24 @@ app.directive('myEnter', function () {
             }
         });
     };
+});
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/patient/:patientId', {
+            templateUrl: 'partials/patient-view.html',
+            controller: 'PatientViewCtrl'
+    })
+    .when('/patients/create', {
+            templateUrl: 'partials/patient-create.html',
+            controller: 'PatientCreateCtrl'
+    })
+    .otherwise({
+        redirectTo: '/patients/create'
+    });
+
+    // configure html5 to get links working on jsfiddle
+    //$locationProvider.html5Mode(true);
 });
 
 app.controller('PatientListCtrl', function ($scope, $firebaseArray) {
@@ -28,12 +46,10 @@ app.controller('PatientListCtrl', function ($scope, $firebaseArray) {
 app.controller('PatientCreateCtrl', function ($scope, $firebaseArray) {
   
   var refPatiens = new Firebase("https://angularclinic.firebaseio.com/patients");
-  // download the data into a local object
-  var getKey = function() {
-    return   $scope.newpatient.surname + $scope.newpatient.name;
-  };
+  
   $scope.createNewPatient = function() {        
         $firebaseArray(refPatiens).$add($scope.newpatient);
+        Materialize.toast("Usuario " + $scope.newpatient.name + " creado", 5000);
         $scope.resetNewPatient();
   };
   $scope.resetNewPatient = function() {
@@ -41,5 +57,26 @@ app.controller('PatientCreateCtrl', function ($scope, $firebaseArray) {
         $scope.newpatient.surname = "";
         $scope.newpatient.email = "";
         $scope.newpatient.phone = "";
+  };
+});
+
+app.controller('PatientViewCtrl', function ($scope, $firebaseObject, $routeParams) {
+  
+  var refPatient = new Firebase("https://angularclinic.firebaseio.com/patients/" + $routeParams.patientId);
+  
+  $scope.patient = $firebaseObject(refPatient);
+  
+  $scope.updatePatient = function() {
+      $scope.patient.$save();      
+      Materialize.toast("Usuario actualizado", 5000);
+  };
+  
+  $scope.confirmDeletePatient = function() {
+     jQuery("#modalConfirmDelete").openModal();
+  };
+  
+  $scope.deletePatient = function() {
+     Materialize.toast("Usuario " + $scope.patient.name + " " + $scope.patient.surname + "eliminado", 5000);
+     $scope.patient.$remove();
   };
 });
