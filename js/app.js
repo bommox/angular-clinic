0aaff0cacd7fe10deda00fb31e5385c7b71b9d99
Jorge <bommox@gmail.com>
2015-12-30 17:13:01 +0100

    Birthday notification

--- js/app.js
+++ js/app.js
@@ -44,6 +44,17 @@ app.controller('PatientListCtrl', function ($scope, $firebaseArray) {
   var ref = new Firebase("https://angularclinic.firebaseio.com/patients");
   // download the data into a local object
   $scope.patients = $firebaseArray(ref);
+  $scope.daysToBirthday = function(date) {
+    if (date === undefined) 
+        return 999;
+    var today = new Date();
+    date = new Date(date);
+    date.setYear(today.getFullYear());
+    var days = Math.round(Math.min(today-date)/(1000*60*60*24));
+    if (days > 180) 
+        days -= 365;
+    return Math.abs(days);
+  };
   $scope.submitNewName = function() {
         $scope.patients.$add($scope.newName);
         $scope.newName = "";
@@ -56,14 +67,14 @@ app.controller('PatientCreateCtrl', function ($scope, $firebaseArray, toast, $lo
   
   $scope.newpatient = $scope.newpatient || {};
   $scope.newpatient.pinned = false;
+  $scope.newpatient.birthInput = new Date();
         
-  $scope.createNewPatient = function() {        
+  $scope.createNewPatient = function() {  
+      $scope.newpatient.birth = $scope.newpatient.birthInput.getTime();
         $firebaseArray(refPatiens).$add($scope.newpatient).then(function(ref) {
             toast("Usuario " + $scope.newpatient.name + $scope.newpatient.surname + " creado");
             $location.path("/patient/" + ref.key());
         });
-        
-        $scope.resetNewPatient();
   };
   $scope.resetNewPatient = function() {
         $scope.newpatient.name = "";
@@ -77,13 +88,21 @@ app.controller('PatientViewCtrl', function ($scope, $firebaseObject, $routeParam
   
   var refPatient = new Firebase("https://angularclinic.firebaseio.com/patients/" + $routeParams.patientId);
   
-  $scope.patient = $firebaseObject(refPatient);
+  $scope.patient = $firebaseObject(refPatient).$loaded().then(function(data) {
+      $scope.patient = data;
+      $scope.patient.birthInput = new Date($scope.patient.birth);
+      console.log(data);
+  }).catch(function() {console.error("Error");});
+  
   $scope.patient.pinned = false;
   
   $scope.updatePatient = function() {
-      $scope.patient.$save();
-      $scope.patientViewForm.$setPristine(); 
-      toast("Usuario actualizado", 1200);
+      $scope.patient.birth = $scope.patient.birthInput.getTime();
+      $scope.patient.$save().then(function() {
+            $scope.patient.birthInput = new Date($scope.patient.birth);
+            $scope.patientViewForm.$setPristine(); 
+            toast("Usuario actualizado", 1200);
+      });
   };
   
   $scope.confirmDeletePatient = function() {
